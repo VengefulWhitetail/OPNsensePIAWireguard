67707ca05998832531b7b9025bd3e49cf8bff8e9
VengefulWhitetail <104092108+VengefulWhitetail@users.noreply.github.com>
2025-02-20 17:24:08 -0500

    Merged class files back into main script.

--- PIAWireguard.py
+++ PIAWireguard.py
@@ -33,6 +33,7 @@ import sys
 import time
 import urllib3
 import secrets
+from abc import ABC, abstractmethod
 
 from PIAWireguardConfigFileLoader import PIAWireguardConfigFileLoader
 
@@ -257,6 +258,27 @@ class State:
     metaIp = ''
     wgCn = ''
     wgIp = ''
+
+class PIAWireguardConfigLoader(ABC):
+
+	@abstractmethod
+	def is_valid(self) -> bool:
+		pass
+
+	@abstractmethod
+	def load_config(self) -> str:
+		pass
+
+class PIAWireguardConfigFileLoader(PIAWireguardConfigLoader):
+	def __init__(self, path: str):
+		self.path = path
+
+	def is_valid(self) -> bool:
+		return os.path.isfile(self.path)
+
+	def load_config(self) -> str:
+		with open(self.path, 'r') as f:
+			return f.read()
 
 # Fixes bug in python requests where this env is preferred over Verify=False
 if 'REQUESTS_CA_BUNDLE' in os.environ:
