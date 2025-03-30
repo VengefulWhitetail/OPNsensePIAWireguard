2f58a862dde59089e2e3ad1c195673073f10fb70
VengefulWhitetail <vw4github@abine.org>
2025-03-29 23:10:20 -0400

    Added exception catch in client cert check.

--- PIAWireguard.py
+++ PIAWireguard.py
@@ -229,8 +229,12 @@ class PIAWireguardConfigURILoader(PIAWireguardConfigLoader):
             logger.critical("Unable to load Base64 data as certificate. This should not happen!")
             return False
 
-        if OID_CLIENT_AUTH not in cert.extensions.get_extension_for_class(ExtendedKeyUsage).value:
-            logger.error("Currently loaded certificate is not a client certificate.")
+        try:
+            if OID_CLIENT_AUTH not in cert.extensions.get_extension_for_class(ExtendedKeyUsage).value:
+                logger.error("Currently loaded certificate is not a client certificate.")
+                return False
+        except Exception:
+            logger.error("Currently loaded certificate has no EKU extension.")
             return False
 
         try:
